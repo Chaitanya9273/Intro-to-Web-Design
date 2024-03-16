367f5fbad41a89ef9623990310eb51ef6a7dc389
Chaitanya Prajapati <chaitanyaprajapati2002@gmail.com>
2024-03-15 22:43:53 -0400

    Initial commit

---
+++ home.js
@@ -0,0 +1,5 @@
+document.querySelector(".clickMe").addEventListener("click",() => {
+    document.querySelectorAll(".hidden").forEach((item) => {
+        item.classList.toggle("showing");
+    });
+});
